eff88dce4865d330fb7f477740e0bf26748f807d
Jean <jeanpolo15@gmail.com>
2025-02-21 22:13:37 -0500

    Add: Constantes para rutas, Sidebar para navegación, iconos

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,7 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({ children }) {
+
+    const loggedIn = {firstName: 'Jean', lastName: 'Polo'}
+
     return (
-        <main>
-            SIDEBAR
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar 
+                user={loggedIn}
+            />
             {children}
         </main>
     );
